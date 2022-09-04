bf490ae843e5e0c9b98df36fefc2c1e40c1ac50e
JUYOUNG Kim <devbat@naver.com>
2022-09-04 22:41:29 +0900

    202209042240

---
+++ src/main/java/com/kh/myapp3/domain/dao/PEventDAOImpl.java
@@ -0,0 +1,32 @@
+package com.kh.myapp3.domain.dao;
+
+import com.kh.myapp3.domain.PEvent;
+
+import java.util.List;
+
+public class PEventDAOImpl implements PEventDAO{
+    @Override
+    public PEvent save(PEvent pEvent) {
+        return null;
+    }
+
+    @Override
+    public void update(Long productId, PEvent pEvent) {
+
+    }
+
+    @Override
+    public PEvent findById(Long mt20id) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long mt20id) {
+
+    }
+
+    @Override
+    public List<PEvent> findAll() {
+        return null;
+    }
+}
